fb1ca351f9b4d6f358cf0ef7c5d8433ef47483b8
Renuka kale <85407443+Renu63@users.noreply.github.com>
2024-05-25 00:23:16 +0530

    Add files via upload
    25 may

---
+++ Ascendingorder.java
@@ -0,0 +1,14 @@
+package java1_8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ascendingorder {
+
+	public static void main(String[] args) {
+		List<Integer> l = Arrays.asList(20, 15, 8, 49, 25, 98, 12, 15, 2, 98, 12, 15, 2, 98, 20);
+
+		l.stream().sorted().forEach(f -> System.out.println(f));
+	}
+
+}
